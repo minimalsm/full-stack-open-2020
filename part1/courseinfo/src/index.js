08cf0288485845a95fe3d0b87b59f1266d9a5f93
Joshua <30259508@cityofglacol.ac.uk>
2020-05-30 17:40:28 +0100

    Completed 1.6

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -36,8 +36,9 @@ const Total = ({ parts }) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+  name: 'Half Stack application development',
+  parts: [
     {
       name: 'Fundamentals of React',
       exercises: 10
@@ -51,12 +52,13 @@ const App = () => {
       exercises: 14
     }
   ]
+  } 
 
   return (
     <>
-      <Header title={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Header title={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </>
   )
 }
