5506982d37a744663533e9613e1d4eef685755e3
Joshua <30259508@cityofglacol.ac.uk>
2020-05-30 13:07:10 +0100

    Completed 1.1

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = props => {
+  return (
+    <>
+      <h1>{props.title}</h1>
+    </>
+  )
+}
+
+const Content = props => {
+  return (
+    <>
+      <p>{props.part} {props.exercise}</p>
+    </>
+  )
+}
+
+const Total = props => {
+  return (
+    <>
+      <p>Number of exercises {props.exercise1 + props.exercise2 + props.exercise3}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <>
+      <Header title={course}/>
+      <Content part={part1} exercise={exercises1}/> 
+      <Content part={part2} exercise={exercises2}/> 
+      <Content part={part3} exercise={exercises3}/> 
+      <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3}/>
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
