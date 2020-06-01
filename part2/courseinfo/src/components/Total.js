34190a20953264948f7da6b252d078cd4c8e76b4
Joshua <30259508@cityofglacol.ac.uk>
2020-06-01 17:04:55 +0100

    Completed 2.1

---
+++ part2/courseinfo/src/components/Total.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Total = ({ parts }) => {
+  return (
+    <div>
+      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+    </div>
+  )
+}
+
+export default Total
