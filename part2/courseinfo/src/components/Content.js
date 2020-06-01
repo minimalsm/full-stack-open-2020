9d00597ce030c46f4c2593706aae93b466211b49
Joshua <30259508@cityofglacol.ac.uk>
2020-06-01 17:34:37 +0100

    Completed 2.5

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -3,7 +3,7 @@ import Part from './Part'
 
 const Content = ({parts}) => {
   const part = parts.map(part => 
-    <Part part={part.name} exercise={part.exercises} />
+    <Part part={part.name} exercise={part.exercises} key={part.id} />
    )
 
   return (
