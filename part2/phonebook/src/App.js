e2177e3daf99b294e33ebaa513e7db2a5ba00cf8
Joshua <30259508@cityofglacol.ac.uk>
2020-06-02 16:23:43 +0100

    Completed 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -9,12 +9,18 @@ const Person = ({ person }) => {
 }
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '07533001122' },
-    { name: 'Alvar Aalto', number: '07533001123' }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filterName, setFilterName ] = useState('')
+  const [ showAll, setShowAll ] = useState()
+
 
 
   const handleNewName = (e) => {
@@ -23,6 +29,12 @@ const App = () => {
 
   const handleNewNumber = (e) => {
     setNewNumber(e.target.value)
+  }
+
+  const handleNewSearch = (e) => {
+    setFilterName(e.target.value)
+
+    filterName ? setShowAll(false) : setShowAll(true)
   }
 
   const isDuplicate = () => persons.some(person => person.name === newName)
@@ -44,9 +56,21 @@ const App = () => {
     setNewName('')
   }
 
+  const peopleToShow = showAll ? persons : persons.filter(p => p.name.includes(filterName))
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+      search
+        <input
+          value={filterName}
+          onChange={handleNewSearch}
+        />
+      </div>
+
+      <p>debug new search: {filterName}</p>
 
       <form onSubmit={addPerson}>
         <div>
@@ -68,7 +92,8 @@ const App = () => {
 
       <h2>Numbers</h2>
       <div>
-          {persons.map(person => <Person person={person} key={person.name} />)}
+          
+          {peopleToShow.map(person => <Person person={person} key={person.name} />)}
       </div>
     </div>
   )
