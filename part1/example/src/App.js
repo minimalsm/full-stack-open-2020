636de65dba2128c1dc94cab32862583a1acab7c5
Joshua <30259508@cityofglacol.ac.uk>
2020-06-06 14:48:04 +0100

    Finished Example Exercise

---
+++ part1/example/src/App.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react'
+import Note from './components/Note'
+import noteService from './services/notes'
+
+
+const App = () => {
+const [notes, setNotes] = useState([])
+const [newNote, setNewNote] = useState('')
+const [showAll, setShowAll] = useState(true)
+
+useEffect(() => {
+  noteService
+    .getAll()
+    .then(initialNotes => {
+      setNotes(initialNotes)
+    })
+}, [])
+
+const toggleImportanceOf = (id) => {
+  const note = notes.find(n => n.id === id)
+  const changedNote = { ...note, important: !note.important }
+
+  noteService
+    .update(id, changedNote)
+    .then(returnedNote => {
+      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+    })
+}
+
+const addNote = (event) => {
+  event.preventDefault()
+  const noteObject = {
+    content: newNote,
+    date: new Date().toISOString(),
+    important: Math.random() < 0.5,
+  }
+
+  noteService
+  .create(noteObject)
+    .then(returnedNote => {
+      setNotes(notes.concat(returnedNote))
+      setNewNote('')
+    })
+}
+
+const handleNoteChange = (event) => {
+  console.log(event.target.value)
+  setNewNote(event.target.value)
+}
+
+
+const notesToShow = showAll 
+  ? notes 
+  : notes.filter(note => note.important)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note, i) => 
+          <Note 
+            key={i} 
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)} 
+          />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+          <input 
+            value={newNote} 
+            onChange={handleNoteChange}
+          />
+          <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
---
+++ part1/example/src/services/notes.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/notes'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+// const getAll = () => {
+//   return axios.get(baseUrl)
+// }
+
+// const create = newObject => {
+//   return axios.post(baseUrl, newObject)
+// }
+
+// const update = (id, newObject) => {
+//   return axios.put(`${baseUrl}/${id}`, newObject)
+// }
+
+export default { 
+  getAll: getAll, 
+  create: create, 
+  update: update 
+}
