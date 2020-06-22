7825003bd9852a2b9e58f34b354c735313a5397e
Joshua <30259508@cityofglacol.ac.uk>
2020-06-22 15:50:25 +0100

    Finished State of the forms

---
+++ notes/src/components/NoteForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const NoteForm = ({ createNote }) => {
+  const [newNote, setNewNote] = useState('')
+
+  const addNote = (event) => {
+    event.preventDefault()
+    createNote({
+      content: newNote,
+      // date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+    })
+
+    setNewNote('')
+  }
+
+  const handleChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Create a new note</h2>
+      
+      <form onSubmit={addNote}>
+      <input 
+        value={newNote}
+        onChange={handleChange}
+      />
+      <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default NoteForm
