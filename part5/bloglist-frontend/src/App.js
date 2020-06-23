17ec9e978ff70b21ebd5e410094424aa23fc23b1
Joshua <30259508@cityofglacol.ac.uk>
2020-06-23 12:37:40 +0100

    Completed 5.9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -119,13 +119,15 @@ const App = () => {
     )
   }
 
+  const blogsSortedLikes = [...blogs].sort((a, b) => b.likes - a.likes)
+ 
   return (
     <div>
       <h2>blogs</h2>
       <Notification message={errorMessage} />
       <p>{user.name} logged in</p> <button onClick={handleLogout}>log out</button>
       {blogForm()}
-      {blogs.map(blog =>
+      {blogsSortedLikes.map(blog =>
           <Blog key={blog.id} blog={blog} handleLike={handleLike} />
       )}
     </div>
