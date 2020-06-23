f80e1800487c5b596ee48e98ea12ca0282348d4b
Joshua <30259508@cityofglacol.ac.uk>
2020-06-23 13:33:03 +0100

    Add Prop-Types

--- notes/src/App.js
+++ notes/src/App.js
@@ -9,88 +9,90 @@ import loginService from './services/login'
 import NoteForm from './components/NoteForm'
 
 const App = () => {
-const [notes, setNotes] = useState([])
-const [showAll, setShowAll] = useState(true)
-const [errorMessage, setErrorMessage] = useState(null)
-const [user, setUser] = useState(null)
+  const [notes, setNotes] = useState([])
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [user, setUser] = useState(null)
+  const noteFormRef = React.createRef()
 
-useEffect(() => {
-  noteService
-    .getAll()
-    .then(initialNotes => {
-      setNotes(initialNotes)
-    })
-}, [])
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
 
-useEffect(() => {
-  const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
-  if(loggedUserJSON) {
-    const user = JSON.parse(loggedUserJSON)
-    setUser(user)
-    noteService.setToken(user.token)
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if(loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
   }
-}, [])
 
-const toggleImportanceOf = (id) => {
-  const note = notes.find(n => n.id === id)
-  const changedNote = { ...note, important: !note.important }
-
-  noteService
-    .update(id, changedNote)
-    .then(returnedNote => {
-      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-    })
-    .catch(error => {
-      setErrorMessage(
-        `Note '${note.content}' was already removed from server`
-      )
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
-      setNotes(notes.filter(n => n.id !== id))
-    })
-}
-
-const addNote = (noteObject) => {
-  noteService
-    .create(noteObject)
-    .then(returnedNote => {
-      setNotes(notes.concat(returnedNote))
-    })
-}
-
-const loginUser = async (userObject) => {
-  try {
-    const user = await loginService.login(userObject)
-    console.log(user)
-    window.localStorage.setItem(
-      'loggedNoteappUser', JSON.stringify(user)
-    ) 
-    noteService.setToken(user.token)
-    setUser(user) 
-  } catch (exception) {
-      setErrorMessage('Wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
+  const addNote = (noteObject) => {
+    noteFormRef.current.toggleVisibility()
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+      })
   }
-}
 
-const notesToShow = showAll 
-  ? notes 
-  : notes.filter(note => note.important)
+  const loginUser = async (userObject) => {
+    try {
+      const user = await loginService.login(userObject)
+      console.log(user)
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      ) 
+      noteService.setToken(user.token)
+      setUser(user) 
+    } catch (exception) {
+        setErrorMessage('Wrong credentials')
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+    }
+  }
 
-const loginForm = () => (
-  <Togglable buttonLabel='login'>
-    <LoginForm createUser={loginUser} />
-  </Togglable> 
-)
+  const notesToShow = showAll 
+    ? notes 
+    : notes.filter(note => note.important)
 
-const noteForm = () => (
-  <Togglable buttonLabel="new note">
-    <NoteForm createNote={addNote} />
-  </Togglable>
-)
+  const loginForm = () => (
+    <Togglable buttonLabel='login'>
+      <LoginForm createUser={loginUser} />
+    </Togglable> 
+  )
+
+  const noteForm = () => (
+    <Togglable buttonLabel="new note" ref={noteFormRef}>
+      <NoteForm createNote={addNote} />
+    </Togglable>
+  )
 
   return (
     <div>
