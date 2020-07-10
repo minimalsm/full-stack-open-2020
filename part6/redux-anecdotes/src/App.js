9c9e0027f9748e9b011af619a3afcbef2e90f4ea
Joshua <30259508@cityofglacol.ac.uk>
2020-07-10 12:39:21 +0100

    Completed 6.7

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { vote, addAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
@@ -32,11 +33,7 @@ const App = () => {
           </div>
         </div>
       )}
-      <h2>create new</h2>
-      <form onSubmit={onAddAnecdote}>
-        <div><input name='anecdote' /></div>
-        <button>create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   )
 }
---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { addAnecdote } from '../reducers/anecdoteReducer'
+
+
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const onAddAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(addAnecdote(content))
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={onAddAnecdote}>
+        <div><input name='anecdote' /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
