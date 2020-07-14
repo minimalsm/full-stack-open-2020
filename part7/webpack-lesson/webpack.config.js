7a62f3ceed599dab4739797dce7d98553bcfbec9
Joshua <30259508@cityofglacol.ac.uk>
2020-07-14 18:20:32 +0100

    Added loaders

---
+++ part7/webpack-lesson/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
+}
+module.exports = config
